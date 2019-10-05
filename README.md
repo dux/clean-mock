2b0aded8ada859e17a71cd4d67eb6478733a9f52
Dino Reic <rejotl@gmail.com>
2019-10-05 11:09:39 +0200

    readme and spec improvment

--- README.md
+++ README.md
@@ -125,3 +125,17 @@ obj.foo                 # :bar
 str = mock.build :commmon_name
 str                     # John, Josh or  Mike
 ```
+
+## Development
+
+After checking out the repo, run `bundle install` to install dependencies. Then, run `rspec` to run the tests.
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/solnic/clean-mock.
+This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the
+[Contributor Covenant](http://contributor-covenant.org) code of conduct.
+
+## License
+
+The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
--- spec/tests/clean_spec.rb
+++ spec/tests/clean_spec.rb
@@ -1,65 +1,73 @@
 require 'spec_helper'
 
 describe CleanMock do
-  it 'creates bad object and it fails' do
-    expect { mock.build(:foo_bar) }.to raise_error(ArgumentError)
+  context 'is faling when' do
+    it 'creates a bad object' do
+      expect { mock.build(:foo_bar) }.to raise_error(ArgumentError)
+    end
   end
 
-  it 'creates user object with default name' do
-    expect(mock.build(:user).name).to include 'User '
-  end
-
-  it 'reads options and adds custom email' do
-    email = 'foo@bar'
-    expect(mock.build(:user, email: email).email).to eq email
-  end
-
-  it 'creates user object with custom methods' do
-    user = mock.build(:user)
-    expect(user.class).to      eq User
-    expect(user.say_ok).to     eq 'ok'
-    expect(user.say_not_ok).to eq 'not ok'
-  end
-
-  it 'creates user with admin trait' do
-    user = mock.build(:user)
-    expect(user.class).to    eq User
-    expect(user.is_admin).to eq false
-
-    user = mock.build(:user, :admin)
-    expect(user.class).to    eq User
-    expect(user.is_admin).to eq true
-  end
-
-  it 'creates admin_user with admin trait' do
-    user = mock.build(:admin_user)
-    expect(user.class).to eq User
-    expect(user.is_admin).to eq true
-  end
-
-  it 'creates object without default class' do
-    foo_bar = mock.build(:foo)
-    expect(foo_bar.foo).to eq :bar
-  end
-
-  it 'creates random string' do
-    list = ['John', 'Josh', 'Mike']
-
-    mock.define :string, class: false do
-      list.sample
+  context 'auto creates class' do
+    it 'with default name' do
+      expect(mock.build(:user).name).to include 'User '
     end
 
-    name = mock.build(:string)
+    it 'and reads options and adds custom email' do
+      email = 'foo@bar'
+      expect(mock.build(:user, email: email).email).to eq email
+    end
 
-    expect(list.include?(name)).to be true
+    it 'and creates user object with custom methods' do
+      user = mock.build(:user)
+      expect(user).to            be_a User
+      expect(user.say_ok).to     eq 'ok'
+      expect(user.say_not_ok).to eq 'not ok'
+    end
+
+    it 'and creates user object with admin trait' do
+      user = mock.build(:user)
+      expect(user).to        be_a User
+      expect(user.is_admin).to eq false
+
+      user = mock.build(:user, :admin)
+      expect(user).to        be_a User
+      expect(user.is_admin).to eq true
+    end
+
+    it 'and links mocked object to org' do
+      user = mock.build(:user)
+      expect(user.org_id).to be_nil
+
+      user = mock.build(:user, :with_org)
+      expect(user.org.is_saved).to be true
+      expect(user.org_id.class).to be Integer
+    end
   end
 
-  it 'links mocked object to org' do
-    user = mock.build(:user)
-    expect(user.org_id).to be_nil
+  context 'manualy defined class (class: User)' do
+    it 'and creates admin_user with admin trait' do
+      user = mock.build(:admin_user)
+      expect(user).to        be_a User
+      expect(user.is_admin).to eq true
+    end
+  end
 
-    user = mock.build(:user, :with_org)
-    expect(user.org.is_saved).to be true
-    expect(user.org_id.class).to be Integer
+  context 'using no class creation (class: false)' do
+    it 'creates object' do
+      foo_bar = mock.build(:foo)
+      expect(foo_bar.foo).to eq :bar
+    end
+
+    it 'creates random string' do
+      list = ['John', 'Josh', 'Mike']
+
+      mock.define :string, class: false do
+        list.sample
+      end
+
+      name = mock.build(:string)
+
+      expect(list.include?(name)).to be true
+    end
   end
 end
